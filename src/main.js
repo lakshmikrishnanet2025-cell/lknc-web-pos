e65e500c3d89694751d4cb255f9931c0d6715b91
raindroptaxi <lakshmikrishnanet2025@gmail.com>
2025-12-06 20:03:36 +0530

    Resolve merge conflict in main.js

--- src/main.js
+++ src/main.js
@@ -1,15 +1,13 @@
-<<<<<<< HEAD
+
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import "./index.css";
 
 createApp(App).use(router).mount("#app");
-=======
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import "./index.css";
 
 createApp(App).use(router).mount("#app");
->>>>>>> 690e282eb5642abfbac3004b594c5b45310af133
