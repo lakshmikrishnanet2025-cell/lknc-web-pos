3d21a10a2d33d88378eb42fae5879eb84809e01a
raindroptaxi <lakshmikrishnanet2025@gmail.com>
2025-12-06 20:00:32 +0530

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,13 @@
-<<<<<<< HEAD
+
 export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
 };
-=======
 export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
 };
->>>>>>> 690e282eb5642abfbac3004b594c5b45310af133
