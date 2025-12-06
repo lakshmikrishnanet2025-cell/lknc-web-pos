4e0e8ad3ec64fa710722f0f86773d0c1514aa918
raindroptaxi <lakshmikrishnanet2025@gmail.com>
2025-12-06 19:57:33 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,19 +8,3 @@ export default defineConfig({
     port: 5173
   }
 });
-
----
-
-# 📌 **vite.config.js**
-
-```js
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-
-export default defineConfig({
-  plugins: [vue()],
-  server: {
-    host: true,
-    port: 5173
-  }
-});
