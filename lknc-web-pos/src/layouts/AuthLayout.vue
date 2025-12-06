91222fe26f7784068ae49607ab102c9558cc336a
raindroptaxi <lakshmikrishnanet2025@gmail.com>
2025-12-06 18:44:05 +0530

    Delete lknc-web-pos directory

--- lknc-web-pos/src/layouts/AuthLayout.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="h-screen flex justify-center items-center bg-gray-200">
-    <div class="bg-white p-6 rounded shadow w-80">
-      <slot/>
-    </div>
-  </div>
-</template>
